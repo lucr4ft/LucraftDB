61f12da7c7aae09f30522d41e3e9dd027ec4d1ce
Luca Lewin <luca.lewin23@gmail.com>
2020-12-21 19:31:57 +0100

    create class Startup

---
+++ Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lucraft.Database
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+    }
+}
