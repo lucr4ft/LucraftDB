501e1ef6ccda60e40e78bc916d0c7ddd456f80b7
Luca Lewin <luca.lewin23@gmail.com>
2020-12-22 17:12:29 +0100

    create Environments

---
+++ Environment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucraft.Database
+{
+    public class DevlopmentEnvironment : IEnvironment
+    {
+        public bool IsDevelopment()
+        {
+            return true;
+        }
+    }
+
+    public class ProductionEnvironment : IEnvironment
+    {
+        public bool IsDevelopment()
+        {
+            return false;
+        }
+    }
+}
---
+++ IEnvironment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lucraft.Database
+{
+    public interface IEnvironment
+    {
+        public bool IsDevelopment();
+    }
+}
