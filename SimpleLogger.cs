c3ab94d8c6d1cb55d7ce43c481e15d986154ad6d
Luca Lewin <luca.lewin23@gmail.com>
2020-12-22 17:08:15 +0100

    add debug field + option

--- SimpleLogger.cs
+++ SimpleLogger.cs
@@ -6,10 +6,13 @@ namespace Lucraft.Database
     public class SimpleLogger
     {
 
+        public static bool Debug = false;
+
         public static void Log(Level lvl, string s)
         {
             if (lvl == Level.INFO) Console.WriteLine($"[{DateTime.Now}.{DateTime.Now:fff}] [{Thread.CurrentThread.Name}/INFO]: {s}");
             else if (lvl == Level.WARN) Console.WriteLine($"[{DateTime.Now}.{DateTime.Now:fff}] [{Thread.CurrentThread.Name}/WARN]: {s}");
+            else if (lvl == Level.DEBUG && Debug) Console.WriteLine($"[{DateTime.Now}.{DateTime.Now:fff}] [{Thread.CurrentThread.Name}/DEBUG]: {s}");
             else if (lvl == Level.ERROR) Console.Error.WriteLine($"[{DateTime.Now}.{DateTime.Now:fff}] [{Thread.CurrentThread.Name}/ERROR]: {s}");
         }
 
@@ -19,7 +22,7 @@ namespace Lucraft.Database
     {
         INFO = 0,
         WARN = 1,
-        ERROR = 2
+        DEBUG = 2,
+        ERROR = 3
     }
-
 }
