367d7d38d42b74833342eadcac8b8ccbb1b1a36b
Luca Lewin <luca.lewin23@gmail.com>
2020-12-23 17:34:30 +0100

    add DataOptions object to represent object in config.json

---
+++ Config/DataOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Lucraft.Database.Config
+{
+    public class DataOptions
+    {
+        [JsonPropertyName("allow-memory-storage")]
+        public bool AllowMemoryStorage { get; init; }
+        [JsonPropertyName("allow-async-read")]
+        public bool AllowAsyncRead { get; init; }
+        [JsonPropertyName("allow-async-write")]
+        public bool AllowAsyncWrite { get; init; }
+    }
+}
