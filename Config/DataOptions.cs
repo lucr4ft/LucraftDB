9fda8e42c4c8f14401904420647308e8e18e4cd2
Luca Lewin <66569227+lucalewin@users.noreply.github.com>
2020-12-29 21:54:25 +0100

    remove unused imports (#2)

--- Config/DataOptions.cs
+++ Config/DataOptions.cs
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+﻿namespace Lucraft.Database.Config
+{
+    public class DataOptions
+    {
+        public bool AllowMemoryStorage { get; init; }
+        public bool AllowAsyncRead { get; init; }
+=======
 ﻿using Newtonsoft.Json;
 
 namespace Lucraft.Database.Config
@@ -9,6 +17,7 @@ namespace Lucraft.Database.Config
         [JsonProperty("allow-async-read")]
         public bool AllowAsyncRead { get; init; }
         [JsonProperty("allow-async-write")]
+>>>>>>> develop
         public bool AllowAsyncWrite { get; init; }
     }
 }
