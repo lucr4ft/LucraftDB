1050f72f638944a9eb8ce74654ad8f384a63e98e
Luca Lewin <luca.lewin23@gmail.com>
2020-12-29 22:40:37 +0100

    create Models namespace + DataModels

---
+++ Models/CollectionResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucraft.Database.Model
+{
+    public class CollectionResponseModel
+    {
+        [JsonProperty("documents")]
+        public List<DocumentResponseModel> Documents { get; init; }
+    }
+}
---
+++ Models/ErrorResponseModel.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Lucraft.Database.Models
+{
+    public class ErrorResponseModel
+    {
+        [JsonProperty("error")]
+        public string Error { get; init; }
+    }
+}
