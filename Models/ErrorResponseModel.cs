4e5360e95ed32bc43b85e071691c43607eb8d2c5
Luca Lewin <luca.lewin23@gmail.com>
2020-12-30 18:05:13 +0100

    extend base class

--- Models/ErrorResponseModel.cs
+++ Models/ErrorResponseModel.cs
@@ -2,7 +2,7 @@
 
 namespace Lucraft.Database.Models
 {
-    public class ErrorResponseModel
+    public class ErrorResponseModel : ResponseModel
     {
         [JsonProperty("error")]
         public string Error { get; init; }
