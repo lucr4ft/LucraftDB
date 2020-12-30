29e8e3e04da3256ddeb865d43fb048777914e0b3
Luca Lewin <luca.lewin23@gmail.com>
2020-12-30 18:12:49 +0100

    create class QueryHandler + add method HandleQuery to handle query requests in a collection

---
+++ Query/QueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Lucraft.Database.Models;
+using System.Collections.Generic;
+
+namespace Lucraft.Database.Query
+{
+    public class QueryHandler
+    {
+        public static QueryResponseModel HandleQuery(Collection collection, string query)
+        {
+            Condition condition = Condition.GetCondition(query);
+            SimpleLogger.Log(Level.DEBUG, condition.ToString());
+            List<DocumentResponseModel> matchingDocs = new List<DocumentResponseModel>();
+            collection.Documents.ForEach(delegate (Document doc)
+            {
+                if (condition.Check(doc)) 
+                    matchingDocs.Add(doc.GetModel());
+            });
+            return new QueryResponseModel { Documents = matchingDocs };
+        }
+    }
+}
