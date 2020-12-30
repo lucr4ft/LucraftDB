4f8271082d3d78484e2e34cb6896bb1066642434
Luca Lewin <luca.lewin23@gmail.com>
2020-12-30 18:29:11 +0100

    remove merge marker

--- Config/DataOptions.cs
+++ Config/DataOptions.cs
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-﻿namespace Lucraft.Database.Config
-{
-    public class DataOptions
-    {
-        public bool AllowMemoryStorage { get; init; }
-        public bool AllowAsyncRead { get; init; }
-=======
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace Lucraft.Database.Config
 {
@@ -14,13 +6,5 @@ namespace Lucraft.Database.Config
     {
         [JsonProperty("allow-memory-storage")]
         public bool AllowMemoryStorage { get; init; }
-<<<<<<< HEAD
-        [JsonProperty("allow-async-read")]
-        public bool AllowAsyncRead { get; init; }
-        [JsonProperty("allow-async-write")]
->>>>>>> develop
-        public bool AllowAsyncWrite { get; init; }
-=======
->>>>>>> develop
     }
 }
