4e74543c79c2c62ac710355cacfa0f0d73dbf338
Luca Lewin <luca.lewin23@gmail.com>
2020-12-31 16:36:52 +0100

    add JsonProperty to fields

--- Config/Configuration.cs
+++ Config/Configuration.cs
@@ -1,10 +1,14 @@
-﻿namespace Lucraft.Database.Config
+﻿using Newtonsoft.Json;
+
+namespace Lucraft.Database.Config
 {
     public class Configuration
     {
         public int Port { get; init; }
+        [JsonProperty("max-connections")]
         public int MaxConnections { get; init; }
         public bool Debug { get; init; }
+        [JsonProperty("data-options")]
         public DataOptions DataOptions { get; init; }
     }
 }
