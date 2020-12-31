5dc3f897de57c310784d2b621164e6bbf0a820bb
Luca Lewin <luca.lewin23@gmail.com>
2020-12-31 17:32:23 +0100

    create Utitlites class for utility methods

---
+++ Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucraft.Database
+{
+    public class Utilities
+    {
+        public static string GetRandomID() => Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Replace("=", "").Replace("/", "").Replace("+", "");
+    }
+}
