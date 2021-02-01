0689e4bde16050acf6ffc9cee67ea56e78b4f815
lucalewin <luca.lewin23@gmail.com>
2021-02-01 16:39:34 +0100

    replace async SocketServer with sync SocketServer

--- SocketServer.cs
+++ SocketServer.cs
@@ -2,61 +2,71 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Lucraft.Database
 {
-    public class SocketServer
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class SocketServer
     {
-        private readonly ManualResetEvent allDone = new(false);
-        private readonly ClientHandler ClientHandler = new();
+        private bool running = false;
 
-        public void Start(int port)
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly IPEndPoint localEndPoint = new(IPAddress.Any, DatabaseServer.Config.Port);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly Socket listener = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Start()
         {
-            SimpleLogger.Log(Level.Info, "Starting server....");
-            IPEndPoint localEndPoint = new(IPAddress.Any, port);
-            // Create a TCP/IP socket.  
-            Socket listener = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            // Bind the socket to the local endpoint and listen for incoming connections.  
+            if (running)
+                throw new Exception("Server is already runnig!");
             try
             {
                 listener.Bind(localEndPoint);
-                listener.Listen(100);
-                SimpleLogger.Log(Level.Info, "Server started successful on port " + port);
+                listener.Listen(10);
+
+                running = true;
+
+                SimpleLogger.Log(Level.Info, "Server started successful on port " + DatabaseServer.Config.Port);
+
                 while (true)
                 {
-                    // Set the event to nonsignaled state.  
-                    allDone.Reset();
-                    // Start an asynchronous socket to listen for connections.  
                     SimpleLogger.Log(Level.Info, "Waiting for a client to connect...");
-                    listener.BeginAccept(new AsyncCallback(AcceptClient), listener);
-                    // Wait until a connection is made before continuing.  
-                    allDone.WaitOne();
+                    // Program is suspended while waiting for an incoming connection.  
+                    Socket handler = listener.Accept();
+
+                    var client = new Client(handler);
+                    Task.Run(async () => await ClientHandler.Add(client));
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.ToString());
+                SimpleLogger.Log(Level.Error, e.ToString());
             }
-            Console.WriteLine("\nPress any key to continue...");
-            Console.Read();
         }
 
         public void Shutdown()
         {
-            ClientHandler.DisconnectAll("Shutting down server");
-        }
-
-        public void AcceptClient(IAsyncResult ar)
-        {
-            // Signal the main thread to continue.  
-            allDone.Set();
-            Thread.CurrentThread.Name = "ClientThread#" + DateTimeOffset.Now.ToUnixTimeMilliseconds();
-            // Get the socket that handles the client request.  
-            Socket listener = (Socket)ar.AsyncState;
-            Socket handler = listener.EndAccept(ar);
-            SimpleLogger.Log(Level.Info, $"Client connected from {handler.RemoteEndPoint}");
-            ClientHandler.Add(new Client(handler));
+            if (!running)
+                throw new Exception("Server is not runnig");
+            SimpleLogger.Log(Level.Info, "Stopping server...");
+            ClientHandler.DisconnectAll("Stopping server");
+            listener.Shutdown(SocketShutdown.Both);
+            listener.Close();
+            SimpleLogger.Log(Level.Info, "Stopped server");
+            Environment.Exit(0);
         }
     }
 }
