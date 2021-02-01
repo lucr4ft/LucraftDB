df3c8744e0dd14524732cdf28e003bf457458f47
lucalewin <luca.lewin23@gmail.com>
2021-02-01 18:55:27 +0100

    create class Credentials

---
+++ Security/Authentication/Credentials.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucraft.Database.Security.Authentication
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Credentials
+    {
+    }
+}
