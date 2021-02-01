6144222300c32cee2e7c8d8c6cf155e7b05821db
lucalewin <luca.lewin23@gmail.com>
2021-02-01 17:24:15 +0100

    create class Rule

---
+++ Security/Rules/Rule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucraft.Database.Security.Rules
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Rule
+    {
+        public string Name { get; init; }
+    }
+}
