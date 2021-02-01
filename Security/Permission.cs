c346baf25c644200f3ad42dd93339ce715125666
lucalewin <luca.lewin23@gmail.com>
2021-02-01 17:23:50 +0100

    create class Permission + enum Permissions

---
+++ Security/Permission.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucraft.Database.Security
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum Permissions
+    {
+        Read,
+        Write,
+        ReadWrite
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Permission
+    {
+    }
+}
