cbbf4195d9ccdfc57d5275f758dd32cde42fa178
lucalewin <luca.lewin23@gmail.com>
2021-08-20 18:31:28 +0200

    create Lucraft.Database.Exceptions namespace + create class MalformedQueryException

---
+++ src/Exceptions/MalformedQueryException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucraft.Database.Exceptions
+{
+    public class MalformedQueryException : Exception
+    {
+        public MalformedQueryException() { }
+
+        public MalformedQueryException(string message) : base(message) { }
+
+        public MalformedQueryException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
